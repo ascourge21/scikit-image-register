9670f1549defa389c6aa5360f1f27b3539acedcb
Nripes Parajuli <ascourge21@gmail.com>
2019-03-09 10:47:19 -0800

    tab/spaces cleanup, flake cleanup

--- register.py
+++ register.py
@@ -10,112 +10,110 @@ import utils
 # TODO - the transformation can also be made part of lambda?
 
 def im_register_scale(im_fixed, im_moving,
-	pad_size=None, options={}):
-	"""For registering only by re-scaling.
-	"""
-	if pad_size is None:
-		pad_size = (im_moving.shape[0] * 2, im_moving.shape[1] * 2)
+                      pad_size=None, options={}):
+    """For registering only by re-scaling."""
+    if pad_size is None:
+        pad_size = (im_moving.shape[0] * 2, im_moving.shape[1] * 2)
 
-	orig_size = (im_moving.shape[0], im_moving.shape[1])
-	
-	im_fixed = utils.resize_image(im_fixed, pad_size)
-	im_moving = utils.resize_image(im_moving, pad_size) / 255
+    orig_size = (im_moving.shape[0], im_moving.shape[1])
 
-	im_f_shape = im_fixed.shape
-	im_m_shape = im_moving.shape
-	
-	assert im_f_shape == im_m_shape
+    im_fixed = utils.resize_image(im_fixed, pad_size)
+    im_moving = utils.resize_image(im_moving, pad_size) / 255
 
-	# random initialization and assignment
-	scale_init = 1.25
-	error_function = lambda x: utils.ssd_scale(
-		x, im_fixed, im_moving, pad_size)
-	scale_optimized = scipy.optimize.minimize(
-	    error_function, scale_init, method='Nelder-Mead',
-	    options=options, jac=False)
+    im_f_shape = im_fixed.shape
+    im_m_shape = im_moving.shape
 
-	# apply solved registration
-	im_registered = skimage.transform.rescale(
-	    im_moving, scale_optimized.x, multichannel=True,
-	    mode='constant', anti_aliasing=True
-	)
-	
-	return utils.resize_image(im_registered, orig_size)
+    assert im_f_shape == im_m_shape
+
+    # random initialization and assignment
+    scale_init = 1.25
+    error_function = lambda x: utils.ssd_scale(
+        x, im_fixed, im_moving, pad_size)
+    scale_optimized = scipy.optimize.minimize(
+        error_function, scale_init, method='Nelder-Mead',
+        options=options, jac=False)
+
+    # apply solved registration
+    im_registered = skimage.transform.rescale(
+        im_moving, scale_optimized.x, multichannel=True,
+        mode='constant', anti_aliasing=True
+    )
+
+    return utils.resize_image(im_registered, orig_size)
 
 
 def im_register_rotate(im_fixed, im_moving,
-	pad_size=None, options={}, show_figs=False):
-	"""For registering only by rotating.
-	"""
-	if pad_size is None:
-		pad_size = (im_moving.shape[0] * 2, im_moving.shape[1] * 2)
+                       pad_size=None, options={}, show_figs=False):
+    """For registering only by rotating.
+    """
+    if pad_size is None:
+        pad_size = (im_moving.shape[0] * 2, im_moving.shape[1] * 2)
 
-	orig_size = (im_moving.shape[0], im_moving.shape[1])
-	
-	im_fixed = utils.resize_image(im_fixed, pad_size)
-	im_moving = utils.resize_image(im_moving, pad_size) / 255
+    orig_size = (im_moving.shape[0], im_moving.shape[1])
 
-	im_f_shape = im_fixed.shape
-	im_m_shape = im_moving.shape
-	
-	assert im_f_shape == im_m_shape
+    im_fixed = utils.resize_image(im_fixed, pad_size)
+    im_moving = utils.resize_image(im_moving, pad_size) / 255
 
-	# random initialization and assignment
-	rotate_init = 0.2
-	error_function = lambda x: utils.ssd_rotate(
-		x, im_fixed, im_moving, pad_size, show_figs=show_figs)
-	scale_optimized = scipy.optimize.minimize(
-	    error_function, rotate_init, method='Nelder-Mead',
-	    options=options, jac=False)
+    im_f_shape = im_fixed.shape
+    im_m_shape = im_moving.shape
 
-	# apply solved registration
-	im_registered = skimage.transform.rotate(
-		im_moving, angle=scale_optimized.x * 180 / (math.pi),
-		resize=True, mode='constant'
-	)
-	
-	return utils.resize_image(im_registered, orig_size)
+    assert im_f_shape == im_m_shape
+
+    # random initialization and assignment
+    rotate_init = 0.2
+    error_function = lambda x: utils.ssd_rotate(
+        x, im_fixed, im_moving, pad_size, show_figs=show_figs)
+    scale_optimized = scipy.optimize.minimize(
+        error_function, rotate_init, method='Nelder-Mead',
+        options=options, jac=False)
+
+    # apply solved registration
+    im_registered = skimage.transform.rotate(
+        im_moving, angle=scale_optimized.x * 180 / (math.pi),
+        resize=True, mode='constant'
+    )
+
+    return utils.resize_image(im_registered, orig_size)
 
 
 def im_register_affine(im_fixed, im_moving,
-	pad_size=None, options={}, show_figs=False):
-	"""For registering using affine transformation.
-	"""
-	if pad_size is None:
-		pad_size = (int(im_moving.shape[0] * 2),
-					int(im_moving.shape[1] * 2))
+    pad_size=None, options={}, show_figs=False):
+    """For registering using affine transformation.
+    """
+    if pad_size is None:
+        pad_size = (int(im_moving.shape[0] * 2),
+                    int(im_moving.shape[1] * 2))
 
+    orig_size = (im_moving.shape[0], im_moving.shape[1])
 
-	orig_size = (im_moving.shape[0], im_moving.shape[1])
-	
-	im_fixed = utils.resize_image(im_fixed, pad_size)
-	im_moving = utils.resize_image(im_moving, pad_size) / 255
+    im_fixed = utils.resize_image(im_fixed, pad_size)
+    im_moving = utils.resize_image(im_moving, pad_size) / 255
 
-	im_f_shape = im_fixed.shape
-	im_m_shape = im_moving.shape
-	
-	assert im_f_shape == im_m_shape
+    im_f_shape = im_fixed.shape
+    im_m_shape = im_moving.shape
 
-	# random initialization and assignment
-	init_scale = (1., 1.)
-	init_rotation = .1
-	init_shear = 0.0
-	init_translation = (0, 0)
-	params_init = (init_scale[0], init_scale[1], init_rotation,
-	               init_shear, init_translation[0], init_translation[1])
-	error_function = lambda x: utils.ssd_affine(
-		x, im_fixed, im_moving, pad_size, show_figs=show_figs)
-	affine_optimized = scipy.optimize.minimize(
-	    error_function, params_init, method='Nelder-Mead',
-	    options=options, jac=False)
+    assert im_f_shape == im_m_shape
 
-	# apply solved registration
-	regis_tfm = skimage.transform.AffineTransform(
-	    scale=(affine_optimized.x[0], affine_optimized.x[1]),
-	    rotation=affine_optimized.x[2],
-	    shear=affine_optimized.x[3],
-	    translation=(affine_optimized.x[4], affine_optimized.x[5]))
-	im_registered = utils.apply_matrix_tform(
-	        im_moving, regis_tfm)
+    # random initialization and assignment
+    init_scale = (1., 1.)
+    init_rotation = .1
+    init_shear = 0.0
+    init_translation = (0, 0)
+    params_init = (init_scale[0], init_scale[1], init_rotation,
+                   init_shear, init_translation[0], init_translation[1])
+    error_function = lambda x: utils.ssd_affine(
+        x, im_fixed, im_moving, pad_size, show_figs=show_figs)
+    affine_optimized = scipy.optimize.minimize(
+        error_function, params_init, method='Nelder-Mead',
+        options=options, jac=False)
 
-	return utils.resize_image(im_registered, orig_size)
+    # apply solved registration
+    regis_tfm = skimage.transform.AffineTransform(
+        scale=(affine_optimized.x[0], affine_optimized.x[1]),
+        rotation=affine_optimized.x[2],
+        shear=affine_optimized.x[3],
+        translation=(affine_optimized.x[4], affine_optimized.x[5]))
+    im_registered = utils.apply_matrix_tform(
+            im_moving, regis_tfm)
+
+    return utils.resize_image(im_registered, orig_size)
--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ import skimage.util
 import skimage.transform
 
 
-# TODO (nripesh): add tests 
+# TODO (nripesh): add tests
 
 
 def resize_image(im, new_size):
@@ -18,43 +18,43 @@ def resize_image(im, new_size):
     assert orig_row > 0
     assert orig_col > 0
     new_row, new_col = new_size
-    
+
     if new_row > orig_row:  # assuming rows/cols scale similarly
         row_pad_before = math.ceil((new_row - orig_row) / 2)
         row_pad_after = new_row - orig_row - row_pad_before
-        
+
         col_pad_before = math.ceil((new_col - orig_col) / 2)
         col_pad_after = new_col - orig_col - col_pad_before
-        
+
         if len(im.shape) == 3:
-            im_resized = skimage.util.pad(im,
-                ((row_pad_before, row_pad_after),
-                 (col_pad_before, col_pad_after),
-                 (0, 0)), mode='constant',
+            im_resized = skimage.util.pad(
+                im, ((row_pad_before, row_pad_after),
+                     (col_pad_before, col_pad_after),
+                     (0, 0)), mode='constant',
             )
         else:
-            im_resized = skimage.util.pad(im,
-                ((row_pad_before, row_pad_after),
-                 (col_pad_before, col_pad_after)),
+            im_resized = skimage.util.pad(
+                im, ((row_pad_before, row_pad_after),
+                     (col_pad_before, col_pad_after)),
                 mode='constant',
-            )           
+            )
     else:
         row_crop_before = math.ceil((orig_row - new_row) / 2)
-        row_crop_after = orig_row - new_row -  row_crop_before
-        
+        row_crop_after = orig_row - new_row - row_crop_before
+
         col_crop_before = math.ceil((orig_col - new_col) / 2)
         col_crop_after = orig_col - new_col - col_crop_before
-        
+
         if len(im.shape) == 3:
-            im_resized = skimage.util.crop(im,
-                ((row_crop_before, row_crop_after),
-                 (col_crop_before, col_crop_after),
-                 (0, 0))
+            im_resized = skimage.util.crop(
+                im, ((row_crop_before, row_crop_after),
+                     (col_crop_before, col_crop_after),
+                     (0, 0))
             )
         else:
-            im_resized = skimage.util.crop(im,
-                ((row_crop_before, row_crop_after),
-                 (col_crop_before, col_crop_after)),
+            im_resized = skimage.util.crop(
+                im, ((row_crop_before, row_crop_after),
+                     (col_crop_before, col_crop_after)),
             )
     assert im_resized.shape[0] == new_size[0]
     assert im_resized.shape[1] == new_size[1]
@@ -62,11 +62,11 @@ def resize_image(im, new_size):
 
 
 def ssd_scale(scale_factor, im_fixed, im_moving, im_pad_size,
-           xx=None, yy=None, gradient=False):
+              xx=None, yy=None, gradient=False):
     """SSD function only for scaling transformation.
 
         TODO (nripesh) : separate the image difference to different function
-        with other differences also avaialble. 
+        with other differences also avaialble.
         TODO (nripesh): refactor to use the same transformation as in affine.
 
     """
@@ -74,18 +74,18 @@ def ssd_scale(scale_factor, im_fixed, im_moving, im_pad_size,
     if scale_factor < 0:
         scale_factor = [np.abs(np.random.randn())]
         print(scale_factor)
-        
+
     im_moving_regis = skimage.transform.rescale(
         im_moving, scale=scale_factor, multichannel=True,
         mode='constant', anti_aliasing=True
     )
-    
+
     im_movig_regis = resize_image(im_moving_regis, im_pad_size)
     row, col, channel = im_movig_regis.shape
 
     q1 = im_fixed - im_movig_regis
     ssd = np.sum(np.square(q1)) / (row * col * channel)
-    
+
     if xx is None:
         xx, yy = np.meshgrid(
             np.arange(im_pad_size[0]), np.arange(im_pad_size[1]))
@@ -104,10 +104,10 @@ def ssd_scale(scale_factor, im_fixed, im_moving, im_pad_size,
         grad_x = np.multiply(grad_x / scale_factor[0], xx)
         grad_y = np.multiply(grad_y / scale_factor[0], yy)
 
-        grad += ( 
+        grad += (
             - 2 * np.sum(np.multiply(
                 q1[:, :, c], grad_x + grad_y)) / (row * col * channel))
-    
+
     print(scale_factor, ssd, grad)
     return ssd, grad
 
@@ -123,9 +123,9 @@ def ssd_rotate(angle, im_fixed, im_moving, im_pad_size,
         angle = 0
     if angle > math.pi:
         angle = 0
-        
+
     im_moving_regis = skimage.transform.rotate(
-        im_moving, angle= angle * 180 / math.pi,
+        im_moving, angle=angle * 180 / math.pi,
         resize=True, mode='constant'
     )
 
@@ -154,8 +154,8 @@ def ssd_rotate(angle, im_fixed, im_moving, im_pad_size,
 def apply_matrix_tform(im, tform):
     """Affine transforms are applied by calling the wrap function.
 
-    Since the wrap function doesn't transform images about the center, 
-    a combination of a translation and its inverse needs to be applied to 
+    Since the wrap function doesn't transform images about the center,
+    a combination of a translation and its inverse needs to be applied to
     the tform before and after respectively.
     """
     shift_y, shift_x = np.array(im.shape[:2]) / 2.
@@ -185,7 +185,7 @@ def ssd_affine(params, im_fixed, im_moving, im_pad_size,
     if scale_x < 0:
         scale_x = [np.abs(np.random.randn())]
         print(scale_x)
-        
+
     moving_tfm = skimage.transform.AffineTransform(
         scale=(scale_x, scale_y),
         rotation=rotation,
@@ -194,15 +194,17 @@ def ssd_affine(params, im_fixed, im_moving, im_pad_size,
 
     im_moving_regis = apply_matrix_tform(
         im_moving, moving_tfm)
-    
+
     row, col, channel = im_moving_regis.shape
 
     q1 = im_fixed - im_moving_regis
     ssd = np.sum(np.square(q1)) / (row * col * channel)
-    
-    print('scale_x: {:2.2f}, scale_y: {:2.2f}, rotation: {:2.2f}, '.format( scale_x, scale_y, rotation) + 
-          'shear: {:2.2f}, t_x: {:2.2f}, t_y: {:2.2f}, SSD: {:2.3f}'.format(shear, trans_x, trans_y, ssd))
-    
+
+    print('scale_x: {:2.2f}, scale_y: {:2.2f}, rotation: {:2.2f}, '.format(
+        scale_x, scale_y, rotation) +
+          'shear: {:2.2f}, t_x: {:2.2f}, t_y: {:2.2f}, SSD: {:2.3f}'.format(
+        shear, trans_x, trans_y, ssd))
+
     if show_figs:
         plt.figure(1)
         plt.title('target (transformed)')
